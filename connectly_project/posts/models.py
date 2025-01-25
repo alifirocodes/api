064bf4345c94d39bf4e05968de2865f74f754453
Rhynne Gracelle <196156994+alifirocodes@users.noreply.github.com>
2025-01-25 13:05:49 +0800

    Update post model and add comment model to posts/model.py

--- connectly_project/posts/models.py
+++ connectly_project/posts/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
 
 
 
-
+"""
 class Post(models.Model):
     content = models.TextField()  # The text content of the post
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who created the post
@@ -22,3 +22,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content[:50]
+    
+"""
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on Post {self.post.id}"
