1fd2b639d7a87cbf6622a3159321da1921c7e843
Rhynne Gracelle <196156994+alifirocodes@users.noreply.github.com>
2025-01-29 09:23:21 +0800

    Update posts/models.py

--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -3,14 +3,13 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import User
+# from .models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User, Post, Comment
 from .serializers import UserSerializer, PostSerializer, CommentSerializer
-
 
 
 # Create your views here.
@@ -100,4 +99,3 @@ class CommentListCreate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
